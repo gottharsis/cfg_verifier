2a2e1f7119e4031d62a42d1274f897bae5b2acfa
Ayush Nene <ayush3108@gmail.com>
2023-04-10 14:47:12 -0400

    create initial ui

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,39 @@
-const path = require("path");
+const path = require("path")
+
+const isProduction = process.env.NODE_ENV === "production"
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+// const isProduction = false
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.ts",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
-  },
-  resolve: {
-    extensions: [".ts", ".ts"],
-  },
+    mode: isProduction ? "production" : "development",
+    entry: "./src/index.ts",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
 
-  // loaders
-  module: {
-    rules: [
-      {
-        test: /\.tsx?/,
-        use: [
-          { loader: "ts-loader", options: { onlyCompileBundledFiles: true } },
+    // loaders
+    module: {
+        rules: [
+            {
+                test: /\.tsx?/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                        options: { onlyCompileBundledFiles: true },
+                    },
+                ],
+                exclude: /node_modules/,
+            },
         ],
-        exclude: /node_modules/,
-      },
+    },
+    devtool: isProduction ? false : "source-map",
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
     ],
-  },
-};
+}
